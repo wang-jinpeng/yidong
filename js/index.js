2c1b8a0edc83140419a98423e26f83d61edbb528
wang-jinpeng <857245460@qq.com>
2018-01-03 23:55:34 +0800

    yidong

--- js/index.js
+++ js/index.js
@@ -61,3 +61,78 @@
             })
     })
 }
+{
+    {
+        let topbar = document.querySelector('.header');
+        let floors = document.querySelectorAll(".floor");
+        let nav = document.querySelector(".cedaohang");
+        let navs = document.querySelectorAll(".cedaohang li");
+        let toptop = document.querySelector(".back");
+        let spans=document.querySelectorAll(".cedaohang span");
+        let flag = true;
+        console.log(floors);
+        window.onscroll = function() {
+            if(flag) {
+                let st = document.documentElement.scrollTop;
+                if(st > 550) {
+                    topbar.style.display = 'block'
+                } else {
+                    topbar.style.display = 'none'
+                }
+                if(st > 650) {
+                    nav.style.display = "block"
+                } else {
+                    nav.style.display = "none"
+                }
+                floors.forEach(function(ele, index) {
+                    if(st < floors[0].offsetTop) {
+                        for(let i = 0; i < navs.length; i++) {
+                            navs[i].classList.remove("active")
+                        }
+                        navs[0].classList.add("active")
+                    }
+                    if(st >= ele.offsetTop-60) {
+                        for(let i = 0; i <navs.length; i++) {
+                            navs[i].classList.remove("active");
+                            spans[i].classList.remove("active")
+                        }
+                        navs[index].classList.add("active");
+                        spans[index].classList.add("active")
+                    }
+                })
+            }
+        };
+        navs.forEach(function(ele, index) {
+            ele.onclick = function() {
+                flag = false;
+                let st = floors[index].offsetTop;
+                // st -= 60;
+                let now = document.documentElement.scrollTop;
+                let speed = (st - now) * 30 / 300;
+                let time = 0;
+                let t = setInterval(function() {
+                    now += speed;
+                    time += 30;
+                    if(time === 300) {
+                        clearInterval(t);
+                        now = st;
+                        flag = true;
+                    }
+                    document.documentElement.scrollTop = now;
+                }, 30)
+            }
+        });
+        toptop.onclick = function() {
+            let st = document.documentElement.scrollTop;
+            let speed = st * 30 / 500;
+            let t = setInterval(function() {
+                st -= speed;
+                if(st <= 0) {
+                    st = 0;
+                    clearInterval(t)
+                }
+                document.documentElement.scrollTop = st
+            }, 30)
+        }
+    }
+}
